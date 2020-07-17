47ba0be6d2063cfe697d9d0260eaa0d4b5e70676
tomxue <tomxue@outlook.com>
2020-07-17 14:56:03 +0800

    1st commit

---
+++ App36/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App36
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Windows.UI.Core.Preview.SystemNavigationManagerPreview.GetForCurrentView().CloseRequested +=
+                async (sender, args) =>
+                {
+                    args.Handled = true;
+                    MessageDialog dialog = new MessageDialog("Yes or No?");
+                    dialog.Commands.Add(new UICommand("Yes", null));
+                    dialog.Commands.Add(new UICommand("No", null));
+                    dialog.DefaultCommandIndex = 0;
+                    dialog.CancelCommandIndex = 1;
+                    var cmd = await dialog.ShowAsync();
+
+                    if (cmd.Label == "Yes")
+                    {
+                        App.Current.Exit();
+                        Debug.WriteLine(".......................Exit........................");
+                    }
+                };
+        }
+    }
+}
